e26fb912d6d3f206038b88c465d52ad266b72117
LeonardoAbanto <leonardo_abanto@hotmail.com>
2023-05-04 21:29:21 -0500

    terminando MI

--- main.py
+++ main.py
@@ -17,10 +17,30 @@ def ReporteTD(i):
     # Información básica:
     print('Lineas de código: ', radon_proyecto.total_sloc, ' / Archivos: ', radon_proyecto.total_files)
 
-    print(radon_proyecto)
+    # MI:
+    print()
+    mi = radon_proyecto.mi
+    if mi < 10:
+        print('Índice de mantenibilidad: ', str(round(radon_proyecto.mi, 2)), ' - Mantenibilidad baja')
+    elif mi < 20:
+        print('Índice de mantenibilidad: ', str(round(radon_proyecto.mi, 2)), ' - Mantenibilidad media')
+    else:
+        print('Índice de mantenibilidad: ', str(round(radon_proyecto.mi, 2)), ' - Mantenibilidad alta')
 
+    # Módulos con bajo MI
+    print()
+    encontrado = False
+    print('Módulos con baja mantenibilidad:')
+    for modulo in radon_por_modulo:
+        if 20 > modulo.mi > 0:
+            print(modulo.file_name, ' - MI: ', str(modulo.mi))
+            encontrado = True
+    if not encontrado:
+        print('--')
+
+    print()
     # Indicadores por proyecto a considerar:
     # MI, Duplicacion, Interdependencia, CC (metodos / clases), SLOC, Archivos, AC/EC, DIT, Lack of Cohesion,
 
 
-ReporteTD(3)
+ReporteTD(6)
