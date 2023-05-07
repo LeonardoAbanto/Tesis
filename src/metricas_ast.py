bf2ca6a94c3136fc00c824ea84556ad80eeaf501
LeonardoAbanto <leonardo_abanto@hotmail.com>
2023-05-07 00:24:36 -0500

    empezando pruebas de AST (aun no funciona del todo)
    - detección de loops
    - detección de condicionales
    - detección de funciones y parametros
    - detección de clases

--- src/metricas_ast.py
+++ src/metricas_ast.py
@@ -15,7 +15,8 @@ def test(project_dir):
 
     for file_name in python_files:
         # Lee el contenido del archivo de Python
-        with open(file_name, 'r') as f:
+        print(file_name)
+        with open(file_name, 'r', encoding='utf-8') as f:
             source_code = f.read()
 
         # Analiza el código fuente en un árbol de sintaxis abstracta (AST)
@@ -26,8 +27,24 @@ def test(project_dir):
             if isinstance(node, ast.FunctionDef):
                 print(f"Nombre de la función: {node.name}")
                 print(f"Número de líneas de la función: {node.body[-1].lineno - node.body[0].lineno + 1}")
-                print(node.args.lineno)
-                print(node.returns)
             elif isinstance(node, ast.ClassDef):
                 print(f"Nombre de la clase: {node.name}")
                 print(f"Número de líneas de la clase: {node.body[-1].lineno - node.body[0].lineno + 1}")
+
+
+def test_astor(project_dir):
+    # Lista para almacenar los nombres de los archivos Python
+    python_files = []
+
+    # Recorremos el directorio y almacenamos los nombres de los archivos Python
+    for root, dirs, files in os.walk(project_dir):
+        for file in files:
+            if file.endswith('.py'):
+                python_files.append(os.path.join(root, file))
+
+    for file_name in python_files:
+        print(file_name)
+        astor.parse_file(file_name)
+        with open(file_name, 'r', encoding='utf-8') as f:
+            source_code = f.read()
+        astor.dump_tree()
--- tests/ast_test.py
+++ tests/ast_test.py
@@ -1,6 +1,96 @@
-from src import metricas_ast
+# from src import metricas_ast
 
 project_dir = 'C:\\Users\\leona\\Desktop\\TESIS\\ProyectosPython\\eli5-master'
 print(project_dir)
 
-metricas_ast.test(project_dir)
+# metricas_ast.test(project_dir)
+# metricas_ast.test_astor(project_dir)
+
+import ast
+
+
+class MetricCounter(ast.NodeVisitor):
+    def __init__(self):
+        self.loops = 0
+        self.conditionals = 0
+        self.functions = []
+        self.classes = []
+
+    def visit_FunctionDef(self, node):
+        self.functions.append({
+            'name': node.name,
+            'lineno': node.lineno,
+            'params': [arg.arg for arg in node.args.args],
+        })
+        self.generic_visit(node)
+
+    def visit_For(self, node):
+        self.loops += 1
+        self.generic_visit(node)
+
+    def visit_While(self, node):
+        self.loops += 1
+        self.generic_visit(node)
+
+    def visit_If(self, node):
+        self.conditionals += 1
+        self.generic_visit(node)
+
+    def visit_IfExp(self, node):
+        self.conditionals += 1
+        self.generic_visit(node)
+
+    def visit_Try(self, node):
+        self.conditionals += 1
+        self.generic_visit(node)
+
+    def visit_TryFinally(self, node):
+        self.conditionals += 1
+        self.generic_visit(node)
+
+    def visit_TryExcept(self, node):
+        self.conditionals += 1
+        self.generic_visit(node)
+
+    def visit_ClassDef(self, node):
+        class_dict = {
+            'name': node.name,
+            'lineno': node.lineno,
+            'methods': 0,
+            'attributes': 0
+        }
+
+        for item in node.body:
+            if isinstance(item, ast.FunctionDef):
+                class_dict['methods'] += 1
+            elif isinstance(item, ast.Assign):
+                class_dict['attributes'] += 1
+
+        self.classes.append(class_dict)
+        self.generic_visit(node)
+
+
+def count_metrics(file_path):
+    with open(file_path, 'r') as f:
+        code = f.read()
+
+    tree = ast.parse(code)
+    counter = MetricCounter()
+    counter.visit(tree)
+
+    for func in counter.functions:
+        if len(func['params']) > 2:
+            print(
+                f"La función '{func['name']}' en la línea {func['lineno']} tiene más de 2 parámetros: {func['params']}")
+
+    for cls in counter.classes:
+        print(
+            f"La clase '{cls['name']}' en la línea {cls['lineno']} tiene {cls['methods']} métodos y {cls['attributes']} atributos")
+
+    print(f"{counter.loops} loops encontrados")
+    print(f"{counter.conditionals} condicionales encontrados")
+    print(f"{len(counter.functions)} funciones encontradas")
+    print(f"{len(counter.classes)} clases encontradas")
+
+
+count_metrics('C:\\Users\\leona\\Desktop\\TESIS\\ProyectosPython\\eli5-master\\tests\\test_sklearn_transform.py')
