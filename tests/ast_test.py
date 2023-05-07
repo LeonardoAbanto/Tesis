c1db810b6627dea671856a570fc934b0439c577d
LeonardoAbanto <leonardo_abanto@hotmail.com>
2023-05-07 02:29:02 -0500

    se agregan avances de metricas ast: - clausulas de excepcion generales/vacias - métodos de clases - otras métricas relacionadas

--- tests/ast_test.py
+++ tests/ast_test.py
@@ -15,12 +15,15 @@ class MetricCounter(ast.NodeVisitor):
         self.conditionals = 0
         self.functions = []
         self.classes = []
+        self.exceptions = 0
+        self.empty_exceptions = 0
 
     def visit_FunctionDef(self, node):
         self.functions.append({
             'name': node.name,
             'lineno': node.lineno,
             'params': [arg.arg for arg in node.args.args],
+            'total_lines': node.end_lineno - node.lineno + 1
         })
         self.generic_visit(node)
 
@@ -41,15 +44,17 @@ class MetricCounter(ast.NodeVisitor):
         self.generic_visit(node)
 
     def visit_Try(self, node):
-        self.conditionals += 1
+        self.exceptions += 1
         self.generic_visit(node)
 
     def visit_TryFinally(self, node):
-        self.conditionals += 1
+        self.exceptions += 1
         self.generic_visit(node)
 
     def visit_TryExcept(self, node):
-        self.conditionals += 1
+        self.exceptions += 1
+        if not node.body:
+            self.empty_exceptions += 1
         self.generic_visit(node)
 
     def visit_ClassDef(self, node):
@@ -57,12 +62,19 @@ class MetricCounter(ast.NodeVisitor):
             'name': node.name,
             'lineno': node.lineno,
             'methods': 0,
-            'attributes': 0
+            'methods_list': [],
+            'attributes': 0,
+            'total_lines': node.end_lineno - node.lineno + 1
         }
 
         for item in node.body:
             if isinstance(item, ast.FunctionDef):
                 class_dict['methods'] += 1
+                method_dict = {
+                    'name': item.name,
+                    'total_lines': item.end_lineno - item.lineno + 1
+                }
+                class_dict['methods_list'].append(method_dict)
             elif isinstance(item, ast.Assign):
                 class_dict['attributes'] += 1
 
@@ -85,12 +97,18 @@ def count_metrics(file_path):
 
     for cls in counter.classes:
         print(
-            f"La clase '{cls['name']}' en la línea {cls['lineno']} tiene {cls['methods']} métodos y {cls['attributes']} atributos")
+            f"La clase '{cls['name']}' en la línea {cls['lineno']} tiene {cls['methods']} métodos, {cls['attributes']} "
+            f"atributos y {cls['total_lines']} líneas de código")
+        print(f"La clase '{cls['name']}' tiene los siguientes métodos:")
+        for mtd in cls['methods_list']:
+            print(f"El método '{mtd['name']}' tiene '{mtd['total_lines']}' líneas de código")
 
     print(f"{counter.loops} loops encontrados")
     print(f"{counter.conditionals} condicionales encontrados")
     print(f"{len(counter.functions)} funciones encontradas")
     print(f"{len(counter.classes)} clases encontradas")
+    print(f"{counter.exceptions} cláusulas de excepción encontradas")
+    print(f"{counter.empty_exceptions} cláusulas de excepción vacias")
 
 
 count_metrics('C:\\Users\\leona\\Desktop\\TESIS\\ProyectosPython\\eli5-master\\tests\\test_sklearn_transform.py')
