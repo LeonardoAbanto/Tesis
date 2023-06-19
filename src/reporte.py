24d0791ee8b91ba986cac3d4c5a760ddbdb5a0b3
LeonardoAbanto <leonardo_abanto@hotmail.com>
2023-06-19 18:40:13 -0500

    avanzando ordenar (falta)

--- src/reporte.py
+++ src/reporte.py
@@ -15,70 +15,71 @@ def ReporteTD_UI(project_dir):
 
     # Creación y configuración de ventana principal
     ventana = tk.Tk()
+    ventana.geometry("900x800")
     ventana.title("Reporte de Deuda Técnica")
     ventana.configure(background=background_color)
     ventana.grid_rowconfigure(0, weight=1)
     ventana.grid_columnconfigure(0, weight=1)
 
-    # Frame Info Proyecto
-    frame_info_proyecto = tk.Frame(ventana, background='white', borderwidth=2, relief="solid")
-    frame_info_proyecto.grid(row=1, column=0, padx=10, pady=10)
+    # Frame 1
+    frame_1 = tk.Frame(ventana, background=background_color)
+    frame_1.grid(row=1, column=0, sticky="w")
 
+    # Frame Info Proyecto
+    frame_info_proyecto = tk.Frame(frame_1, background='white', borderwidth=2, relief="solid")
+    frame_info_proyecto.grid(row=1, column=1, padx=10, pady=10, sticky="nw")
     # Nombre del proyecto
     frame_titulo = tk.Frame(frame_info_proyecto)
-    frame_titulo.pack()
+    frame_titulo.grid(row=1, column=0, sticky="w")
     nombre_carpeta = os.path.basename(project_dir)
-    label_titulo = tk.Label(frame_titulo, text='Nombre del proyecto:', font=("Arial", 13), background='white')
+    label_titulo = tk.Label(frame_titulo, text='Nombre del proyecto:', font=("Arial", 15), background='white')
     label_titulo.pack(side=tk.LEFT)
-    nombre_proyecto = tk.Label(frame_titulo, text=nombre_carpeta, font=("Arial", 13, "bold"), background='white')
+    nombre_proyecto = tk.Label(frame_titulo, text=nombre_carpeta, font=("Arial", 15, "bold"), background='white')
     nombre_proyecto.pack(side=tk.LEFT)
-
     # Tamaño de proyecto
     frame_sloc = tk.Frame(frame_info_proyecto)
-    frame_sloc.pack()
-    label_sloc = tk.Label(frame_sloc, text='Líneas de codigo:', font=("Arial", 13), background='white')
+    frame_sloc.grid(row=2, column=0, sticky="w")
+    label_sloc = tk.Label(frame_sloc, text='Líneas de codigo:', font=("Arial", 12), background='white')
     label_sloc.pack(side=tk.LEFT)
-    sloc = tk.Label(frame_sloc, text=str(radon_proyecto.total_sloc), font=("Arial", 13, "bold"), background='white')
+    sloc = tk.Label(frame_sloc, text=str(radon_proyecto.total_sloc), font=("Arial", 12, "bold"), background='white')
     sloc.pack(side=tk.LEFT)
     frame_files = tk.Frame(frame_info_proyecto)
-    frame_files.pack()
-    label_files = tk.Label(frame_files, text='Total de archivos python:', font=("Arial", 13), background='white')
+    frame_files.grid(row=3, column=0, sticky="w")
+    label_files = tk.Label(frame_files, text='Total de archivos Python:', font=("Arial", 12), background='white')
     label_files.pack(side=tk.LEFT)
-    files = tk.Label(frame_files, text=str(radon_proyecto.total_files), font=("Arial", 13, "bold"), background='white')
+    files = tk.Label(frame_files, text=str(radon_proyecto.total_files), font=("Arial", 12, "bold"), background='white')
     files.pack(side=tk.LEFT)
 
     # Frame Indicadores
-    frame_indicadores = tk.Frame(ventana, background='white', borderwidth=2, relief="solid")
-    frame_indicadores.grid(row=2, column=0, padx=10, pady=10)
-
+    frame_indicadores = tk.Frame(frame_1, background='white', borderwidth=2, relief="solid")
+    frame_indicadores.grid(row=1, column=2, padx=10, pady=10, sticky="nw")
     # Complejidad (temporal):
     complexity = tk.Label(frame_indicadores, text=('Complejidad total: ' + str(radon_proyecto.total_cc)),
                           font=("Arial", 13), background='white')
-    complexity.pack()
+    complexity.grid(row=1, column=0, sticky="w", pady=2, padx=2)
     # MI:
     frame_mi = tk.Frame(frame_indicadores)
-    frame_mi.pack()
-    label_mi = tk.Label(frame_mi, text='Indice de Mantenibilidad (Proyecto):', font=("Arial", 13), background='white')
+    frame_mi.grid(row=2, column=0, sticky="w", pady=2, padx=2)
+    label_mi = tk.Label(frame_mi, text='Indice de Mantenibilidad:', font=("Arial", 13), background='white')
     label_mi.pack(side=tk.LEFT)
     mi_proyecto = radon_proyecto.mi
     rating_mi = rating_MI(mi_proyecto)
-    mi = tk.Label(frame_mi, text=str(round(mi_proyecto, 2)), font=("Arial", 13), background=ratings_colores[rating_mi])
+    mi = tk.Label(frame_mi, text=str(round(mi_proyecto, 2)), font=("Arial", 12), background=ratings_colores[rating_mi])
     mi.pack(side=tk.LEFT)
     # CMT:
     frame_cmt = tk.Frame(frame_indicadores)
-    frame_cmt.pack()
-    label_cmt = tk.Label(frame_cmt, text='Densidad de Comentarios:', font=("Arial", 13), background='white')
+    frame_cmt.grid(row=3, column=0, sticky="w", pady=2, padx=2)
+    label_cmt = tk.Label(frame_cmt, text='Comentarios:', font=("Arial", 12), background='white')
     label_cmt.pack(side=tk.LEFT)
     cmt_pct = radon_proyecto.total_cmt / radon_proyecto.total_sloc
     rating_cmt = rating_CMT(cmt_pct)
-    cmt = tk.Label(frame_cmt, text="%.2f%%" % (100 * cmt_pct), font=("Arial", 13),
+    cmt = tk.Label(frame_cmt, text="%.2f%%" % (100 * cmt_pct), font=("Arial", 12),
                    background=ratings_colores[rating_cmt])
     cmt.pack(side=tk.LEFT)
-
     # % Módulos CC>60
     frame_cc_mod = tk.Frame(frame_indicadores)
-    frame_cc_mod.pack()
-    label_cc_mod = tk.Label(frame_cc_mod, text='Módulos con alta complejidad:', font=("Arial", 13), background='white')
+    frame_cc_mod.grid(row=4, column=0, sticky="w", pady=2, padx=2)
+    label_cc_mod = tk.Label(frame_cc_mod, text='Módulos con alta complejidad:', font=("Arial", 12), background='white')
     label_cc_mod.pack(side=tk.LEFT)
     cc_modulo_count = 0
     for modulo in radon_por_modulo:
@@ -86,14 +87,13 @@ def ReporteTD_UI(project_dir):
             cc_modulo_count += 1
     pct_cc_modulos = cc_modulo_count / len(radon_por_modulo)
     rating_cc_mod = rating_cc(pct_cc_modulos)
-    cc_mod = tk.Label(frame_cc_mod, text="%.2f%%" % (100 * pct_cc_modulos), font=("Arial", 13),
+    cc_mod = tk.Label(frame_cc_mod, text="%.2f%%" % (100 * pct_cc_modulos), font=("Arial", 12),
                       background=ratings_colores[rating_cc_mod])
     cc_mod.pack(side=tk.LEFT)
-
     # % Módulos CC>60
     frame_cc_met = tk.Frame(frame_indicadores)
-    frame_cc_met.pack()
-    label_cc_met = tk.Label(frame_cc_met, text='Métodos con alta complejidad:', font=("Arial", 13), background='white')
+    frame_cc_met.grid(row=5, column=0, sticky="w", pady=2, padx=2)
+    label_cc_met = tk.Label(frame_cc_met, text='Métodos con alta complejidad:', font=("Arial", 12), background='white')
     label_cc_met.pack(side=tk.LEFT)
     function_count = 0
     cc_function_count = 0
@@ -104,18 +104,17 @@ def ReporteTD_UI(project_dir):
                 cc_function_count += 1
     pct_cc_metodos = cc_function_count / function_count
     rating_cc_met = rating_cc(pct_cc_metodos)
-    cc_met = tk.Label(frame_cc_met, text="%.2f%%" % (100 * pct_cc_metodos), font=("Arial", 13),
+    cc_met = tk.Label(frame_cc_met, text="%.2f%%" % (100 * pct_cc_metodos), font=("Arial", 12),
                       background=ratings_colores[rating_cc_met])
     cc_met.pack(side=tk.LEFT)
 
     # Frame Módulos MI
     frame_low_mi = tk.Frame(ventana, background='white', borderwidth=2, relief="solid")
-    frame_low_mi.grid(row=3, column=0, padx=10, pady=10)
-
+    frame_low_mi.grid(row=3, column=0, padx=10, pady=10, sticky="w")
     # Módulos con bajo MI
     low_mi_encontrado = False
-    titulo = tk.Label(frame_low_mi, text='Módulos con baja mantenibilidad:', font=("Arial", 13), background='white')
-    titulo.pack()
+    titulo = tk.Label(frame_low_mi, text='Módulos con baja mantenibilidad:', font=("Arial", 14, "bold"), background='white')
+    titulo.grid(sticky="w")
     for modulo in radon_por_modulo:
         if 20 > modulo.mi > 0:
             low_mi = tk.Label(frame_low_mi,
@@ -124,18 +123,18 @@ def ReporteTD_UI(project_dir):
                                     "%.2f%%" % modulo.mi_params[3] + ', LOC: ' + str(modulo.mi_params[2]) + ', HV: ' +
                                     str(round(modulo.mi_params[0], 2))),
                               font=("Arial", 13), background='white')
-            low_mi.pack()
+            low_mi.grid(sticky="w")
             low_mi_encontrado = True
     if not low_mi_encontrado:
-        not_found = tk.Label(frame_low_mi, text='-', font=("Arial", 13), background='white')
-        not_found.pack()
+        not_found = tk.Label(frame_low_mi, text='-', font=("Arial", 12), background='white')
+        not_found.grid(sticky="w")
 
     # Frames smells
     frame_smells_resumido = tk.Frame(ventana, background='white', borderwidth=2, relief="solid")
-    frame_smells_resumido.grid(row=4, column=0, padx=10, pady=10)
+    frame_smells_resumido.grid(row=4, column=0, padx=10, pady=10, sticky="w")
 
     frame_smells_detalle = tk.Frame(ventana, background=background_color)
-    frame_smells_detalle.grid(row=5, column=0)
+    frame_smells_detalle.grid(row=5, column=0, padx=10, pady=10, sticky="w")
 
     # Crear un widget de desplazamiento vertical
     scrollbar_y = tk.Scrollbar(frame_smells_detalle, orient=tk.VERTICAL)
@@ -147,7 +146,7 @@ def ReporteTD_UI(project_dir):
 
     # Crear un widget de texto para información de smells
     text_widget = tk.Text(frame_smells_detalle, yscrollcommand=scrollbar_y.set, xscrollcommand=scrollbar_x.set,
-                          font=("Arial", 13), background='white')
+                          font=("Arial", 13), background='white', width=300)
     text_widget.grid(row=0, column=0, sticky=tk.NSEW)
 
     scrollbar_y.config(command=text_widget.yview)
@@ -179,16 +178,15 @@ def ReporteTD_UI(project_dir):
             total_count[key] = total_count.get(key, 0) + value
 
     # Contadores Smells
-
     if total_count:
-        resumen_smells_titulo = tk.Label(frame_smells_resumido, text='Code Smells encontrados:', font=("Arial", 13),
-                                         background='white')
-        resumen_smells_titulo.pack()
+        resumen_smells_titulo = tk.Label(frame_smells_resumido, text='Code Smells detectados:',
+                                         font=("Arial", 14, "bold"), background='white')
+        resumen_smells_titulo.grid(column=0, sticky="w")
         for key, value in total_count.items():
             if value > 0:
-                smells_contador = tk.Label(frame_smells_resumido, text=(key + ' - ' + str(value)), font=("Arial", 13),
+                smells_contador = tk.Label(frame_smells_resumido, text=(key + ' - ' + str(value)), font=("Arial", 12),
                                            background='white')
-                smells_contador.pack()
+                smells_contador.grid(column=0, sticky="w")
 
     text_widget.insert(tk.END, '\n'.join(smells_array))
     text_widget.config(state="disabled")
